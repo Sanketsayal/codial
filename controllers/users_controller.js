38e0f7eee1efe3719d097684622ac2039dc13bd2
sanket sayal <snktsayal@gmail.com>
2023-02-08 10:58:40 +0530

    sign up page done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,44 @@
+const User=require('../models/user')
 module.exports.profile=function(req,res){
     return res.end('<h1>user profile</>');
+}
+
+module.exports.signup=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codial|sign up"
+    })
+}
+
+module.exports.signin=function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codial|sign in"
+    })
+}
+
+module.exports.create=function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('/');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log('error in finding user');
+            return;
+        }
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log('error in creating user');
+                    return;
+                }
+                return res.redirect('/users/sign-in')
+            })
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
+}
+
+module.exports.createSession=function(req,res){
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const router=express.Router();
 
-const user_controller=require('../controllers/users_controller')
+const userController=require('../controllers/users_controller')
 
-router.get('/profile',user_controller.profile);
+router.get('/profile',userController.profile);
 
-
+router.get('/sign-up',userController.signup);
+router.get('/sign-in',userController.signin);
+router.post('/create',userController.create);
 module.exports=router;
